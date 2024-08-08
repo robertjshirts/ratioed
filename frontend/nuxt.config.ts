81eff62e42237e4ef948a0e4f8fcf0ddb6b72e5d
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-08 03:14:12 -0600

    cleanup

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-08-07',
   modules: ['@nuxt/ui'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
