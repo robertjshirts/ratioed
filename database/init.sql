f37a54c05686158c32b5b3cb7adfbb92d9ff2ef4
robertjshirts <333221.rob@gmail.com>
2024-08-08 18:13:43 -0600

    add attachment table to db

--- database/init.sql
+++ database/init.sql
@@ -4,7 +4,8 @@ CREATE TABLE account (
     email VARCHAR(100) NOT NULL UNIQUE,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
-    bio TEXT NOT NULL
+    bio TEXT NOT NULL DEFAULT '',
+    pfp VARCHAR(255)
 );
 
 -- Create Post table
@@ -21,7 +22,16 @@ CREATE TABLE post (
     FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
 );
 
+-- Create Attachment table
+-- id only exists to differentiate here, no foreign keys link to it
+CREATE TABLE attachment (
+    id SERIAL PRIMARY KEY,
+    parent_id NOT NULL,
+    src VARCHAR(255) NOT NULL,
+);
+
 -- Create Hashtag table
+-- Composite key because hashtags should be unique on any post
 CREATE TABLE hashtag (
     parent_id INT NOT NULL,
     tag VARCHAR(50) NOT NULL,
@@ -30,12 +40,13 @@ CREATE TABLE hashtag (
 );
 
 -- Create Reaction table
+-- Composite key because there should only be one reaction per post
 CREATE TABLE reaction (
-    id SERIAL PRIMARY KEY,
     reaction_type VARCHAR(10) NOT NULL CHECK (reaction_type IN ('like', 'dislike')),
-    parent_id INT,
+    parent_id INT NOT NULL,
     account_id INT NOT NULL,
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (parent_id, account_id),
     FOREIGN KEY (parent_id) REFERENCES post(id) ON DELETE CASCADE,
     FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
 );
@@ -47,3 +58,4 @@ CREATE INDEX idx_post_timestamp ON post(timestamp);
 CREATE INDEX idx_reaction_parent_id ON reaction(parent_id);
 CREATE INDEX idx_reation_type ON reaction(reaction_type);
 CREATE INDEX idx_hashtag_tag ON hashtag(tag);
+CREATE INDEX idx_attachment_parent_id ON attachment(parent_id)
--- database/test.sql
+++ database/test.sql
@@ -4,6 +4,7 @@ DELETE FROM reaction;
 DELETE FROM hashtag;
 DELETE FROM post;
 DELETE FROM account;
+DELETE FROM attachment;
 
 -- Insert test data into account table
 INSERT INTO account (id, email, username, password, bio) VALUES
@@ -14,10 +15,13 @@ INSERT INTO account (id, email, username, password, bio) VALUES
 -- Insert test data into post table
 INSERT INTO post (id, parent_id, body, account_id, ratioed, timestamp) VALUES
 (10, NULL, 'This is the first post by Alice', 10, FALSE, CURRENT_TIMESTAMP),
-(11, 10, 'This is a comment on the first post by Bob', 11, FALSE, CURRENT_TIMESTAMP),
+(11, 10, 'This is a comment on Alice''s first post by Bob', 11, FALSE, CURRENT_TIMESTAMP),
 (12, NULL, 'This is another post by Carol', 12, FALSE, CURRENT_TIMESTAMP),
-(13, 12, 'This is a comment on the second post by Alice', 10, FALSE, CURRENT_TIMESTAMP),
-(14, NULL, 'This is a third post by Bob', 11, TRUE, CURRENT_TIMESTAMP);
+(13, 12, 'This is a comment on the Carol''s post by Alice', 10, FALSE, CURRENT_TIMESTAMP),
+(14, NULL, 'This is a third post by Bob, with an attachment.', 11, TRUE, CURRENT_TIMESTAMP);
+
+INSERT INTO attachment (parent_id, src) VALUES
+(14, 'https://preview.redd.it/the-sacabambaspis-an-extinct-genus-of-jawless-fish-v0-mn6hrm3yk5sa1.jpg?auto=webp&s=fff00db26e0b861d1a4beff64ae3398a11aa68a1');
 
 -- Insert test data into hashtag table
 INSERT INTO hashtag (parent_id, tag) VALUES
@@ -28,15 +32,15 @@ INSERT INTO hashtag (parent_id, tag) VALUES
 (14, 'thirdpost');
 
 -- Insert test data into reaction table
-INSERT INTO reaction (id, reaction_type, parent_id, account_id, timestamp) VALUES
-(10, 'like', 10, 11, CURRENT_TIMESTAMP),
-(11, 'dislike', 10, 12, CURRENT_TIMESTAMP),
-(12, 'like', 12, 10, CURRENT_TIMESTAMP),
-(13, 'dislike', 12, 11, CURRENT_TIMESTAMP),
-(14, 'like', 14, 12, CURRENT_TIMESTAMP);
+INSERT INTO reaction (reaction_type, parent_id, account_id, timestamp) VALUES
+('like', 10, 11, CURRENT_TIMESTAMP),
+('dislike', 10, 12, CURRENT_TIMESTAMP),
+('like', 12, 10, CURRENT_TIMESTAMP),
+('dislike', 12, 11, CURRENT_TIMESTAMP),
+('like', 14, 12, CURRENT_TIMESTAMP);
 
 -- Select statements to retrieve data from each table
-SELECT * FROM account;
-SELECT * FROM post;
-SELECT * FROM hashtag;
-SELECT * FROM reaction;
+-- SELECT * FROM account;
+-- SELECT * FROM post;
+-- SELECT * FROM hashtag;
+-- SELECT * FROM reaction;
