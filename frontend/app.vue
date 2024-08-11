4221807779572d088f342ebb25bb5e2b8a866c75
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-11 01:39:28 -0600

    cleanup

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="fixed z-40 w-full bg-[#09090b] px-4">
+  <div class="fixed z-40 w-full px-4">
     <div class="flex items-center justify-between p-3">
       <span class="text-2xl font-semibold">ratioed</span>
+      <Button>click me</Button>
     </div>
   </div>
-  <div class="pt-14">
-    <NuxtPage />
-  </div>
+  <div class="pt-14"></div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,17 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
-  modules: ["@nuxt/icon", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxt/icon", "@nuxtjs/tailwindcss", "@nuxt/ui"],
   compatibilityDate: "2024-08-07",
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: "",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: "./components/ui",
-  },
 });
