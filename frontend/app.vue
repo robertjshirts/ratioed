9f5b6642323920b9c385523a3e61b6d130a38550
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-10 22:33:55 -0600

    update styling

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,9 +2,11 @@
 
 <template>
   <div class="fixed z-40 w-full border bg-white px-4">
-    <div class="flex justify-between p-3">
+    <div class="flex items-center justify-between p-3">
       <span class="text-2xl font-semibold">ratioed</span>
-      <div><input type="text"  class="rounded-full"/></div>
+      <div>
+        <input type="text" class="rounded-full px-3" />
+      </div>
       <button>login</button>
     </div>
   </div>
