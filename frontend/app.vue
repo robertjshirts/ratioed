2043ec1256e0047e4d0753e1687f3ea6d98a2855
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-14 20:47:17 -0600

    setup navbar

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,13 +1,4 @@
 <template>
-  <div
-    class="fixed top-0 z-40 w-full border-b border-[#272727] bg-[#151515] p-3 text-center"
-  >
-    <span class="text-2xl">ratioed</span>
-  </div>
-  <div class="container mx-auto mt-24">
-    <Sidebar />
-    <div class="ms-72 flex flex-col p-3">
-      <NuxtPage class="rounded-xl" />
-    </div>
-  </div>
+  <Navbar />
+  <Sidebar />
 </template>
---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="fixed top-0 flex w-full items-center justify-between border-b border-[#3f3f3f] px-6 py-4"
+  >
+    <span class="text-2xl font-bold">ratioed</span>
+    <div class="flex items-center">
+      <button class="rounded-md bg-white px-6 py-1 text-black">Log in</button>
+      <button class="ms-4 flex">
+        <Icon name="carbon:overflow-menu-vertical" class="text-2xl" />
+      </button>
+    </div>
+  </div>
+</template>
