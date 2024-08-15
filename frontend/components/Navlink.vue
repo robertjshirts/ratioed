af1d8a790c0300e20e7a09c35c4abc5d602653a6
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-15 02:16:50 -0600

    update styling

--- frontend/components/Navlink.vue
+++ frontend/components/Navlink.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 <template>
   <NuxtLink
     :to="to"
-    class="hover: flex w-full items-center rounded-lg p-2 text-xl font-bold text-gray-400 transition-all"
+    class="flex w-full items-center rounded-lg p-2 text-xl font-bold text-gray-400 transition-all hover:text-gray-200"
     :class="{ 'text-white': $route.path == props.to }"
   >
     <Icon
--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-const status = ref(false);
+const status = true;
 </script>
-/
 
 <template>
   <div class="fixed top-16 box-border h-full w-60">
-    <div v-if="status" class="flex flex-col items-center pb-8">
-      <span>welcome back,</span>
+    <div v-if="status" class="flex flex-col py-8">
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUxlLWqyirlf0ApuqexYrx3jH9P031hUiCOw&s"
         alt=""
         class="w-24 rounded-full"
       />
-      <span class="mt-4 text-lg">Heavener Fan</span>
+      <div class="flex flex-col">
+        <span class="mt-4 text-lg">Heavener Fan</span>
+        <span class="mt-2 text-lg text-gray-400">@metalcoreenjoyer</span>
+      </div>
     </div>
     <nav class="border-b border-[#3f3f3f] py-5" :class="{ 'border-y': status }">
       <Navlink to="/home" active-icon="ph:house-fill" inactive-icon="ph:house"
@@ -36,6 +37,9 @@ const status = ref(false);
         >log in to follow users, ratio others, and have bad takes</span
       >
       <button class="mb-2 mt-4 rounded-md border py-2">Log in</button>
+    </div>
+    <div v-if="status" class="pt-8">
+      <button class="w-full rounded-3xl bg-white py-3 text-black">Post</button>
     </div>
   </div>
 </template>
