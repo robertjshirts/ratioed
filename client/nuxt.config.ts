5a33e492d718f281628153fd99dac3fd2eff1695
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-16 16:09:42 -0600

    update lock

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,12 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-08-07",
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/icon",
-    "@nuxtjs/google-fonts",
-    "@nuxtjs/supabase",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
       Outfit: true,
