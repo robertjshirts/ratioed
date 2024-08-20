bd25c1b25dc816f632514789633dc28f7ba9297c
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-20 14:03:17 -0600

    ROBBIE I ADDED A SIGN OUT BUTTON :D

--- client/composables/useProfile.ts
+++ client/composables/useProfile.ts
@@ -1,9 +1,17 @@
 export default function useProfile() {
-  return useState<{
-    username: string;
-    avatarUrl: string | null;
-  }>("profile", () => ({
-    username: "",
-    avatarUrl: "",
-  }));
+  const username = useState<string>("username", () => "");
+  const avatarUrl = useState<string>("avatarUrl", () => "");
+
+  async function signOut() {
+    const supabase = useSupabaseClient();
+    const { error } = await supabase.auth.signOut();
+
+    if (error) return;
+
+    username.value = "";
+    avatarUrl.value = "";
+    reloadNuxtApp();
+  }
+
+  return { username, avatarUrl, signOut };
 }
