e0a9142f5cc6c77ea08e20b1692fb1240b8abd09
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-20 11:10:33 -0600

    add profile composable

--- client/composables/useProfile.ts
+++ client/composables/useProfile.ts
@@ -1,7 +1,9 @@
 export default function useProfile() {
-  const username = useState("username", () => "");
-  const email = useState("email", () => "");
-  const avatarUrl = useState("avatarUrl", () => "");
-
-  return { username, email, avatarUrl };
+  return useState<{
+    username: string;
+    avatarUrl: string | null;
+  }>("profile", () => ({
+    username: "",
+    avatarUrl: "",
+  }));
 }
--- client/pages/confirm.vue
+++ client/pages/confirm.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
+import type { Database } from "~/types/database";
+
 const user = useSupabaseUser();
 
 watch(
   user,
-  () => {
+  async () => {
     if (user.value) {
+      const supabase = useSupabaseClient<Database>();
+
+      const { data } = await supabase
+        .from("profiles")
+        .select(`id, username, avatar_url`)
+        .eq("id", user.value.id)
+        .single();
+
+      if (data) {
+        const profile = useProfile();
+        profile.value.username = data.username;
+        profile.value.avatarUrl = data.avatar_url;
+      }
+
+      reloadNuxtApp();
       return navigateTo("/");
     }
   },
