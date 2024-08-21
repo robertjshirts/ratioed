26f395ebc4909f9d1e8338b450ec4efe54e44d2d
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-21 01:04:04 -0600

    clean up some stuff

--- client/pages/user/[username]/posts/[id].vue
+++ client/pages/user/[username]/posts/[id].vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
-import { usePost } from '~/composables/usePost';
+import { useRoute } from "vue-router";
+import { usePost } from "~/composables/usePost";
 import type { Database } from "~/types/database";
-import PostComponent from '~/components/Post.vue';
-import CommentComponent from '~/components/Comment.vue';
 
 const supabase = useSupabaseClient();
 
 const route = useRoute();
 const post_id = route.params.id as string;
 
-const { post, error, loading } = await usePost(post_id);    
+const { post, error, loading } = await usePost(post_id);
 
 type ParentPostsView = Database["public"]["Views"]["parent_posts_view"];
 type Post = ParentPostsView["Row"];
@@ -21,9 +19,9 @@ const commentsError = ref<string | null>(null);
 watch(post, async (postValue) => {
   if (postValue) {
     const { data, error } = await supabase
-      .from('parent_posts_view')
+      .from("parent_posts_view")
       .select()
-      .eq('parent_id', post_id);
+      .eq("parent_id", post_id);
     if (error) {
       console.error(error);
       commentsError.value = error.message;
@@ -33,16 +31,15 @@ watch(post, async (postValue) => {
     }
   }
 });
-
 </script>
 
 <template>
-  <div class="container mx-auto py-4">
-    <div v-if="loading" class="text-center py-4">
+  <div class="mx-auto py-4">
+    <div v-if="loading" class="py-4 text-center">
       <p>Loading post...</p>
     </div>
 
-    <div v-else-if="error" class="text-center py-4 text-red-500">
+    <div v-else-if="error" class="py-4 text-center text-red-500">
       <p>Error: {{ error }}</p>
     </div>
 
@@ -68,7 +65,11 @@ watch(post, async (postValue) => {
         </div>
         <div v-else>
           <ul>
-            <li v-for="comment in comments" :key="comment.post_id ?? undefined" class="mt-2">
+            <li
+              v-for="comment in comments"
+              :key="comment.post_id ?? undefined"
+              class="mt-2"
+            >
               <CommentComponent
                 :profile_id="comment.user_id"
                 :profiles="{
@@ -85,7 +86,7 @@ watch(post, async (postValue) => {
       </div>
     </div>
 
-    <div v-else class="text-center py-4">
+    <div v-else class="py-4 text-center">
       <p>Post not found</p>
     </div>
   </div>
