dc421876d321bb3ccd6a5e59fea604c32303bf8e
robertjshirts <333221.rob@gmail.com>
2024-08-26 01:24:48 -0600

    add pagination to index page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,17 +7,23 @@ const supabase = useSupabaseClient<Database>();
 let channel: RealtimeChannel;
 let timeline = ref<Post[]>([]);
 
+const limit = 25;
+let offset = 0;
+
+// Fetch timeline onMounted
 onMounted(async function() {
   const { data, error } = await supabase
     .from("posts_view")
     .select(`*`)
     .is("parent_id", null)
-    .order("created_at", { ascending: false });
+    .order("created_at", { ascending: false })
+    .limit(limit);
   timeline.value = data ?? [];
   if (error) {
     console.error(error);
   }
 
+  // Subscribe for realtime posts
   channel = supabase
     .channel("posts")
     .on(
@@ -35,10 +41,28 @@ onMounted(async function() {
   channel.subscribe();
 });
 
+// Unmount the channel
 onUnmounted(() => {
   supabase.removeChannel(channel);
 });
 
+// Load more posts
+async function loadMore() {
+  offset += limit;
+  const { data, error } = await supabase
+    .from("posts_view")
+    .select(`*`)
+    .is("parent_id", null)
+    .order("created_at", { ascending: false })
+    .range(offset, offset + limit - 1);
+  if (error) {
+    console.error(error);
+  }
+
+  timeline.value.push(...(data ?? []));
+}
+
+// Util function
 async function getPostView(post_id: string) {
   const { data, error } = await supabase
     .from("posts_view")
@@ -78,5 +102,15 @@ async function getPostView(post_id: string) {
   </div>
   <div class="me-8 ms-8 md:ms-0 xl:me-72">
     <Post v-for="post in timeline" :key="post.post_id ?? undefined" v-bind="post" />
+    <button
+      v-if="timeline.length >= (offset + limit)"
+      @click="loadMore"
+      class="w-full rounded-full bg-white px-2 py-3 mt-2 mb-10 text-black"
+      >
+      Load more
+    </button>
+    <p v-else class="pt-4 mb-10 text-center">
+      Damn you read all the posts. Touch grass, dude. 
+    </p>
   </div>
 </template>
