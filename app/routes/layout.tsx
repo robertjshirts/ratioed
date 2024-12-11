15286a93ac461cf70e6a3e305a4528bd5369a77f
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-12-11 00:52:05 -0700

    add shadcn button

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
+import { Button } from "~/components/ui/button";
 
 export default function Layout() {
   return (
     <div>
       <nav>navbar</nav>
       <Outlet />
+      <Button>shadcn button!</Button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,18 +5,15 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          '"Inter"',
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
+        sans: ["Inter"],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
