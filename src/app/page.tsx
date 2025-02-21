9715f4c1b29ae05a693cd7266ec77bd90255bc0f
robertjshirts <robertjshirts@gmail.com>
2025-02-20 23:07:33 -0700

    small change to test deploy script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-dvw h-dvh flex items-center justify-center">
-      <span>BORN AGAIN</span>
+      <span>BORN AGAIN!!!</span>
     </div>
   );
 }
